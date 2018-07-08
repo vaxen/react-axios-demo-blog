873ed73778a37ccec267b523e5b09650eea9c983
mpietropaolo <mpp.pietropaolo@gmail.com>
2018-07-08 22:44:19 +0200

    fix route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Blog from './containers/Blog/Blog'
 class App extends Component {
   render () {
     return (
-      <BrowserRouter basename='/'>
+      <BrowserRouter basename='/react-axios-demo-blog'>
         <div className='App'>
           <Blog />
         </div>
